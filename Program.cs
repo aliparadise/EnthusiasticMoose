7d1dad95b9693b4833468c86d407f9b2ea34ce6c
Ali Paradise <alison.paradise@gmail.com>
2022-02-24 13:18:45 -0600

    Phase 5

--- Program.cs
+++ Program.cs
@@ -2,8 +2,33 @@
 
 Console.WriteLine("Welcome to the Enthusiastic Moose Simulator!");
 Console.WriteLine("--------------------------------------------");
+
 MooseSays("You seem really cool!");
 MooseSays("Would you like to be friends?");
+
+bool isTrue = MooseAsks("Is Canada real?");
+Console.WriteLine(isTrue);
+
+bool MooseAsks(string question)
+{
+    Console.Write($"{question} (Y/N): ");
+    string answer = Console.ReadLine().ToLower();
+
+    while (answer != "y" && answer != "n")
+    {
+        Console.Write($"{question} (Y/N): ");
+        answer = Console.ReadLine().ToLower();
+    }
+
+    if (answer == "y")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
 
 
 void MooseSays(string message)
